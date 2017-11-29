0beaba4ea91d4c8da2b1ee7b51ad17aa1584a4bd
rubenperre <ruben.perre@hotmail.com>
2017-11-29 01:24:52 +0100

    add assets map

--- src/main.js
+++ src/main.js
@@ -5,29 +5,33 @@ import Phaser from 'phaser'
 import config from './config'
 
 class Game extends Phaser.Game {
-    constructor() {
-        super( config.gameWidth, config.gameHeight, Phaser.AUTO, 'content', null )
-        this.state.add( 'Game', GameState );
-        this.state.start( 'Game' );
-    }
+  constructor() {
+    super(config.gameWidth, config.gameHeight, Phaser.AUTO, 'content', null)
+    this.state.add('Game', GameState);
+    this.state.start('Game');
+  }
 }
 
 class GameState extends Phaser.State {
-    init() {
-        console.log( `init` );
-    }
-    preload() {
-        console.log( `preload` );
-    }
-    create() {
-        console.log( `create` );
-    }
-    update() {
-        // console.log( `update` );
-    }
-    render() {
-        // console.log( `render` );
-    }
+  init() {
+    console.log(`init`);
+  }
+  preload() {
+    console.log(`preload`);
+    game.load.image(`block`, `assets/images/block.png`);
+  }
+  create() {
+    console.log(`create`);
+    game.add.image(0, 0, 'block');
+
+  }
+  update() {
+    // console.log( `update` );
+  }
+  render() {
+
+    // console.log( `render` );
+  }
 }
 
 window.game = new Game()

c97e7a8915d6b00f2b054072c8434bffbbc0e1e0
Kristof Dewilde <me@krivi.be>
2017-12-12 23:17:06 +0100

    Fix linting errors

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -6,7 +6,7 @@ export default class extends Phaser.State {
 
     preload() {
         this.loaderBg = this.add.sprite( this.game.world.centerX, this.game.world.centerY, 'loaderBg' );
-        this.loaderBar = this.add.sprite( this.game.world.centerX, this.game.world.centerY, 'loaderBar' );;
+        this.loaderBar = this.add.sprite( this.game.world.centerX, this.game.world.centerY, 'loaderBar' );
         centerGameObjects( [ this.loaderBg, this.loaderBar ] );
 
         this.load.setPreloadSprite( this.loaderBar );

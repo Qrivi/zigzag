18ba5dee16fd98a3d17592765f80f0558c36ba8b
Kristof Dewilde <me@krivi.be>
2017-12-12 22:37:02 +0100

    Overlap detection is a bitch. MY BITCH. ✌️😎

--- src/classes/Tile.js
+++ src/classes/Tile.js
@@ -1,57 +1,81 @@
 import Phaser from 'phaser'
 
 export default class Tile extends Phaser.Graphics {
-    constructor( { game, preceder, size, offset } ) {
-        if( preceder ) {
-            super( game, preceder.x + offset, preceder.y - size * .5 );
-            this.size = size;
-            this.offset = offset;
-        } else {
-            super( game, game.width / 2, game.height - 400 );
-            this.size = size * 4;
-            this.offset = 0;
-        }
+    constructor( { game, preceder, size, offset, hotspot } ) {
+        super( game, 0, 0 );
 
         this.game = game;
         this.preceder = preceder;
-        this.hitArea = new Phaser.Rectangle( -size, 0, size * 2, size );
 
-        this.anchor.setTo( .5 );
-        this.drawSide();
-        this.drawSurface();
-    }
+        this.size = ( preceder ? size : size * 4 );
+        this.posX = ( preceder ? preceder.posX + offset : game.width / 2 );
+        this.posY = ( preceder ? preceder.posY - size * .5 : hotspot - size * 2 );
 
-    getBounds() {
-        return this.hitArea;
+        this.hotspot = hotspot;
+        this.accessible = false;
+        this.passed = false;
+
+        this.surface = new Phaser.Polygon( [
+            new Phaser.Point( this.posX, this.posY ),
+            new Phaser.Point( this.posX + this.size, this.posY + this.size * .5 ),
+            new Phaser.Point( this.posX, this.posY + this.size ),
+            new Phaser.Point( this.posX - this.size, this.posY + this.size * .5 )
+        ] );
+
+        this.draw();
     }
 
     update() {
-        if( this.alive && this.y - this.game.camera.y > this.game.height ) {
-            this.kill();
-            this.destroy();
+        if( this.alive ) {
+            const pos = this.posY - this.game.camera.y;
+
+            if( pos > this.game.height ) {
+                this.destroy();
+            } else if( pos > this.hotspot ) {
+                this.accessible = false;
+                this.passed = true;
+            } else if( pos > this.hotspot - this.size ) {
+                this.accessible = true;
+                this.passed = false;
+            } else {
+                this.accessible = false;
+                this.passed = false;
+            }
         }
+
+        // this.draw();
     }
 
-    drawSide() {
-        this.beginFill( 0x6291D4 );
+    contains( points ) {
+        let hits = 0;
+        points.forEach( point => {
+            if( this.surface.contains( point.x, point.y ) )
+                hits++;
+        } );
+        return hits;
+    }
+
+    draw( colorSurface, colorSide ) {
+        if( !colorSurface )
+            colorSurface = 0x89C9FA;
+        if( !colorSide )
+            colorSide = 0x6291D4;
+        this.clear();
+
+        // side
+        this.beginFill( colorSide );
         this.drawPolygon( [
-            new Phaser.Point( this.size, this.size * .5 ),
-            new Phaser.Point( this.size, this.size * 2.5 ),
-            new Phaser.Point( 0, this.size * 3 ),
-            new Phaser.Point( -this.size, this.size * 2.5 ),
-            new Phaser.Point( -this.size, this.size * .5 )
+            new Phaser.Point( this.posX + this.size, this.posY + this.size * .5 ),
+            new Phaser.Point( this.posX + this.size, this.posY + this.size * 2.5 ),
+            new Phaser.Point( this.posX, this.posY + this.size * 3 ),
+            new Phaser.Point( this.posX - this.size, this.posY + this.size * 2.5 ),
+            new Phaser.Point( this.posX - this.size, this.posY + this.size * .5 )
         ] );
         this.endFill();
-    }
 
-    drawSurface() {
-        this.beginFill( 0x89C9FA );
-        this.drawPolygon( [
-            new Phaser.Point( 0, 0 ),
-            new Phaser.Point( this.size, this.size * .5 ),
-            new Phaser.Point( 0, this.size ),
-            new Phaser.Point( -this.size, this.size * .5 )
-        ] );
+        // surface
+        this.beginFill( colorSurface );
+        this.drawPolygon( this.surface );
         this.endFill();
     }
 }
